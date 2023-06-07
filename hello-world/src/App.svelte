d6ddda7a58e674229847d730bfb2bc552b296449
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-07 18:20:05 +0530

    Binding text

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,10 +1,9 @@
 <script>
-	export let name;
+	const name = 'Batman'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {name}</h1>
 </main>
 
 <style>
